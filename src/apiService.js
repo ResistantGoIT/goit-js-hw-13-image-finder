d717013cb70b38e99e36e84199c8a007abdbf373
Vitaliy Protasevych <vitaliy.protasevych@gmail.com>
2021-03-26 22:29:52 +0200

    upgrade

--- src/apiService.js
+++ src/apiService.js
@@ -1,39 +1,41 @@
-let page = 1;
-export default async function fetchImages(input) {
-  const response = await fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${input}&page=${page}&per_page=12&key=20088872-09746933b80002dd45f6f5108`,
-  );
-  const img = response.json();
-  page += 1;
-  return img;
-}
-
-//export default class NewApiService {
-//  constructor() {
-//    this.input = '';
-//    this.page = 1;
-//  }
-
-//  fetchArticles() {
-//    fetch(
-//      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${input}&page=${page}&per_page=12&key=20088872-09746933b80002dd45f6f5108`,
-//    )
-//      .then(response => response.json())
-//      .then(data => {
-//        this.incrementPage();
-//        console.log(this);
-//      });
-//  }
-//  incrementPage() {
-//    this.page += 1;
-//  }
-//  resetPage() {
-//    this.page = 1;
-//  }
-//  get input() {
-//    return this.input;
-//  }
-//  set input(newInput) {
-//    this.input = newInput;
-//  }
+//let page = 1;
+//export default async function fetchImages(input) {
+//  const response = await fetch(
+//    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${input}&page=${page}&per_page=12&key=20088872-09746933b80002dd45f6f5108`,
+//  );
+//  const img = response.json();
+//  page += 1;
+//  return img;
 //}
+
+export default class NewApiService {
+  constructor() {
+    this.input = '';
+    this.page = 1;
+  }
+
+  fetchArticles() {
+    return fetch(
+      `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.input}&page=${this.page}&per_page=12&key=20088872-09746933b80002dd45f6f5108`,
+    ).then(response => {
+      this.incrementPage();
+      return response.json();
+    });
+    //.then(data => {
+    //  this.incrementPage();
+    //  return data.images;
+    //});
+  }
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  get query() {
+    return this.input;
+  }
+  set query(newInput) {
+    this.input = newInput;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import './styles.css';
-import debounce from 'lodash.debounce';
-import fetchImages from './apiService';
-//import NewApiService from './apiService';
+//import fetchImages from './apiService';
+import NewApiService from './apiService';
 import updateImageMarkup from './updateImageMarkup';
 import refs from './refs';
 
-//const newApiService = new NewApiService();
-
+const debounce = require('lodash.debounce');
+const newApiService = new NewApiService();
 refs.inputImages.addEventListener(
   'input',
   debounce(() => {
-    const input = refs.inputImages.value;
+    newApiService.query = refs.inputImages.value;
+    newApiService.resetPage();
     refs.imagesInfo.innerHTML = ' ';
-    //newApiService.fetchArticles().then(images => updateImageMarkup(images));
-    //newApiService.resetPage();
-    fetchImages(input).then(images => updateImageMarkup(images));
+    //newApiService.fetchArticles();
+    newApiService.fetchArticles().then(images => updateImageMarkup(images));
+    //resetPages();
+    //fetchImages(input).then(images => updateImageMarkup(images));
 
     window.addEventListener('scroll', () => {
       if (
         window.scrollY + window.innerHeight >=
         document.documentElement.scrollHeight
       ) {
-        fetchImages(input).then(images => updateImageMarkup(images));
-        //newApiService.fetchArticles().then(images => updateImageMarkup(images));
+        //fetchImages(input).then(images => updateImageMarkup(images));
+        newApiService.fetchArticles().then(images => updateImageMarkup(images));
       }
     });
   }, 1000),
 );
+
+//refs.inputImages.addEventListener('input', onSearch);
+
+//function onSearch(e) {
+//  e.preventDefault();
+//  const input = refs.inputImages.value;
+//  refs.imagesInfo.innerHTML = ' ';
+//  console.log(input);
+//  newApiService.fetchArticles().then(images => updateImageMarkup(images));
+//}
